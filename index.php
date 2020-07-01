bde9c05de1a55b8b4aa3cadfd28c5fab4408447b
Albertaugustine <yesuarielalbert@gmail.com>
2020-07-02 00:50:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+</head>
+<body ng-app="myApp">
+
+    <div class="jumbotron text-center">
+        <h2>WELCOME</h2>
+    
+    </div>
+    <a href="#/!"></a>
+    <div class="jumbotron text-center">
+        <a href="#!land">Click Here</a>
+    </div>    
+
+        
+
+
+<div ng-view></div>
+
+
+<script src="app/route.js"></script>
+</body>
+</html>
